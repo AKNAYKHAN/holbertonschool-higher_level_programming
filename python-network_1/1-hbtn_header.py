00428dda6c706fd8bde7a57d2a347ed391644675
AKNAYKHAN <ayxandmirciyev@gmail.com>
2026-04-17 19:02:43 +0000

    Fix header script with docstring and main guard

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Fetch and display the X-Request-Id header value from a URL."""
+
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
 
-
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get("X-Request-Id"))
