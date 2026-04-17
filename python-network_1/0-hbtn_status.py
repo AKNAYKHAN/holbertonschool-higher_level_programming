3a4765dcdf887c8ed34d1c270371c8f5b33ff84e
AKNAYKHAN <ayxandmirciyev@gmail.com>
2026-04-17 18:57:01 +0000

    Fetch URL status using urllib

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Fetch status using urllib
+
+import urllib.request
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
+
