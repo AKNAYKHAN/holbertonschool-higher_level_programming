5ee33bd32da2189fa4bc4bd01de08e6f6dd1a003
AKNAYKHAN <ayxandmirciyev@gmail.com>
2026-04-17 19:33:40 +0000

    Implement API security with Basic Auth and JWT

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,8 @@
+cd holbertonschool-higher_level_programming/restful-api
+touch task_05_basic_security.py
+nano task_05_basic_security.py
+chmod +x task_05_basic_security.py
+pycodestyle task_05_basic_security.py
+git add task_05_basic_security.py
+git commit -m "Implement API security with Basic Auth and JWT"
+git push
